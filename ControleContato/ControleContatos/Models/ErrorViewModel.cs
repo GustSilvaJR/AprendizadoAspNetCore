fd9620993b1775a22310050cad86b7ae06350a27
Gustavo Alessandro da Silva Júnior <gust.offic@gmail.com>
2022-09-15 08:40:24 -0300

    Delete ControleContato directory

--- ControleContato/ControleContatos/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ControleContatos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
