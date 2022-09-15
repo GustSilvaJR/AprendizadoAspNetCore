ebf3ecc988806e816f58cc539ea3516635492347
GustSilvaJR <gust.offic@gmail.com>
2022-09-15 16:46:49 -0300

    Inserindo required aos atributos do model, adjunto de mensagem de alerta para preenchimento e mensagens de alerta quanto a tipo de email informado errado

--- ControleContato/ControleContatos/Models/UsuarioModel.cs
+++ ControleContato/ControleContatos/Models/UsuarioModel.cs
@@ -1,14 +1,24 @@
 ﻿using ControleContatos.Enum;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace ControleContatos.Models
 {
     public class UsuarioModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do Usuario")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Digite o Login do Usuario")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite o E-mail do contato")]
+        [EmailAddress(ErrorMessage = "O E-mail informado não é válido!")]
         public string Email { get; set; }
         public Perfil PerfilUser { get; set; }
+
+        [Required(ErrorMessage = "Digite a Senha do Usuario")]
         public string Senha { get; set; }
         public string DataCriacao { get; set; }
         public string? DataAtualizacao { get; set; }
