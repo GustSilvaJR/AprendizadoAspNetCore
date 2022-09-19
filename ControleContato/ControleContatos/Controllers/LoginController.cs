7a26ac1158452c6be7a90ead39bf374217f3419d
GustSilvaJR <gust.offic@gmail.com>
2022-09-19 11:02:22 -0300

    Criando controller para o login. Criando rota httpGet que ja redireciona para a view de Login. E uma HttpPost que recebe o formulario enviado na view de Login, contendo o login e a senha. Em seguida cria o hash md5 da senha e passa para a função validaSignIn de usuarioRepositorio para efetuar o login

---
+++ ControleContato/ControleContatos/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using ControleContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            this._usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(IFormCollection value)
+        {
+            try
+            {
+                var hashPass = _usuarioRepositorio.GetHashPass(value["senha"]);
+
+                if (this._usuarioRepositorio.ValidaSignIn(value["login"], hashPass))
+                {
+                    TempData["MensagemSucesso"] = $"Login: {value["login"]} \n | Senha: {value["senha"]} \n Logado com Sucesso!!";
+                    return RedirectToAction("Index", "Home");
+                }
+
+                TempData["MensagemErro"] = $"Login e/ou senha inválidos.";  
+                return RedirectToAction("Index");
+            }
+            catch(System.Exception error)
+            {
+                TempData["MensagemErro"] = $"Não foi possível efetuar o login, tente novamente mias tarde. Detalhe do erro: {error.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
