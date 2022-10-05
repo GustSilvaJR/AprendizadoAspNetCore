a9206382fd806c1dff5d1c41c3d321916d1522eb
GustSilvaJR <gust.offic@gmail.com>
2022-10-05 16:16:01 -0300

    Ajustes finais

--- ControleContato/ControleContatos/Controllers/LoginController.cs
+++ ControleContato/ControleContatos/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using ControleContatos.Repositorio;
+﻿using ControleContatos.Helper;
+using ControleContatos.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleContatos.Controllers
@@ -6,15 +7,19 @@ namespace ControleContatos.Controllers
     public class LoginController : Controller
     {
         private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ISessao _sessao;
 
-        public LoginController(IUsuarioRepositorio usuarioRepositorio)
+        public LoginController(IUsuarioRepositorio usuarioRepositorio, ISessao sessao)
         {
             this._usuarioRepositorio = usuarioRepositorio;
+            this._sessao = sessao;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //Se ja estiver logado, irá redirecionar para a pagina home de uma vez. Sem que seja necessario refazer o login
+            
+            return this._sessao.getSession()!=null ? RedirectToAction("Index","Home") : View("Index");
         }
 
         [HttpPost]
@@ -26,6 +31,9 @@ namespace ControleContatos.Controllers
 
                 if (this._usuarioRepositorio.ValidaSignIn(value["login"], hashPass))
                 {
+                    var usuario = _usuarioRepositorio.GetUserByLogin(value["login"]);
+                    _sessao.createSession(usuario);
+
                     TempData["MensagemSucesso"] = $"Login: {value["login"]} \n | Senha: {value["senha"]} \n Logado com Sucesso!!";
                     return RedirectToAction("Index", "Home");
                 }
@@ -38,6 +46,12 @@ namespace ControleContatos.Controllers
                 TempData["MensagemErro"] = $"Não foi possível efetuar o login, tente novamente mias tarde. Detalhe do erro: {error.Message}";
                 return RedirectToAction("Index");
             }
+        }
+    
+        public IActionResult Logout()
+        {
+            this._sessao.deleteSession();
+            return RedirectToAction("Index");
         }
     }
 }
